88c3d6fe1364b37644f37f96c67ffcf4dd7e85fc
Tony <a.mitev098@gmail.com>
2024-03-11 23:59:10 +0100

    Commit 1 division

---
+++ src/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public double dividir(double num1, double num2){
+        if(num2 == 0){
+            return 0;
+        } else{
+            return num1 / num2;
+        }
+
+    }
+
+
+}
