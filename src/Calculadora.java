fc1ab065993a5b803cee13c33b2fda7f5fe090d2
Tony <a.mitev098@gmail.com>
2024-03-12 00:55:39 +0100

    Commit main fix sintaxis

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,19 +1,36 @@
 public class Calculadora {
+    /** Método dividir
+     @param num1 Primer número a dividir.
+     @param num2 Segundo número a dividir.
+     @return Devuelve la división de los números. */
     public double dividir(double num1, double num2){
         if(num2 == 0){
             return 0;
         } else{
             return num1 / num2;
         }
-
     }
 
+    /** Método sumar
+     @param num1 Primer número a sumar.
+     @param num2 Segundo número a sumar.
+     @return Devuelve la suma de los números. */
     public double sumar(double num1, double num2){
         return num1 + num2;
     }
+
+    /** Método resta
+     @param num1 Primer número a restar.
+     @param num2 Segundo número a restar.
+     @return Devuelve la resta de los números. */
     public double resta(double num1, double num2){
         return num1 - num2;
     }
+
+    /** Método multiplicar
+     @param num1 Primer número a multiplicar.
+     @param num2 Segundo número a multiplicar.
+     @return Devuelve la multiplicación de los números. */
     public double multiplicar(double num1, double num2){
         return num1 * num2;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Calculadora calcular = new Calculadora();
+        Scanner sc = new Scanner(System.in);  // Importamos la clase Scanner.
+        Calculadora calcular = new Calculadora(); // Creamos un objeto de tipo calculadora.
         double num1 = 0;
         double num2 = 0;
 
-        int opcion;
+        int opcion; // Creamos una variable de tipo entero llamada opción, que nos servirá más adelante para introducir los parámetros de nuestro menú.
         do{
             System.out.println("Menu calculadora:");
             System.out.println("1. - Sumar");
@@ -16,43 +16,47 @@ public class Main {
             System.out.println("3. - Multiplicar");
             System.out.println("4. - Dividir");
             System.out.println("0. - Salir");
-            System.out.print("Introduce una opcion: ");
+            System.out.println("Introduce una opcion: ");
             opcion = sc.nextInt();
 
-            switch (opcion){
+            switch (opcion){ // Creamos un caso para las diferentes opciones.
                 case 1:
-                    System.out.print("Introduce el primer numero");
+                    // CASO 1: Ejecuta el método sumar.
+                    System.out.print("Introduce el primer numero: ");
                     num1 = sc.nextDouble();
-                    System.out.print("Introduce el segundo numero");
+                    System.out.print("Introduce el segundo numero: ");
                     num2 = sc.nextDouble();
-                    System.out.print(calcular.sumar(num1, num2));
+                    System.out.println(calcular.sumar(num1, num2));
                     break;
                 case 2:
-                    System.out.print("Introduce el primer numero");
+                    // CASO 1: Ejecuta el método restar.
+                    System.out.print("Introduce el primer numero: ");
                     num1 = sc.nextDouble();
-                    System.out.print("Introduce el segundo numero");
+                    System.out.print("Introduce el segundo numero: ");
                     num2 = sc.nextDouble();
-                    System.out.print(calcular.resta(num1, num2));
+                    System.out.println(calcular.resta(num1, num2));
                     break;
                 case 3:
-                    System.out.print("Introduce el primer numero");
+                    // CASO 1: Ejecuta el método multiplicar.
+                    System.out.print("Introduce el primer numero: ");
                     num1 = sc.nextDouble();
-                    System.out.print("Introduce el segundo numero");
+                    System.out.print("Introduce el segundo numero: ");
                     num2 = sc.nextDouble();
-                    System.out.print(calcular.multiplicar(num1, num2));
+                    System.out.println(calcular.multiplicar(num1, num2));
                     break;
                 case 4:
+                    // CASO 1: Ejecuta el método dividir.
                     System.out.print("Introduce el primer numero");
                     num1 = sc.nextDouble();
                     System.out.print("Introduce el segundo numero");
                     num2 = sc.nextDouble();
-                    System.out.print(calcular.dividir(num1, num2));
+                    System.out.println(calcular.dividir(num1, num2));
                     break;
                 case 0:
+                    // CASO 0: Sale del programa.
                     System.out.println("Cierras el programa");
                     break;
             }
-
-        } while(opcion != 0);
+        } while(opcion != 0);  // El bucle se ejecuta hasta que la opción sea igual a 0.
     }
 }
